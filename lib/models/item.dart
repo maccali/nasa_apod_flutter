61b4c0d61a2e646e2c1404a45a882517de260ba4
maccali <guilherme.maccali@universo.univates.br>
2019-12-17 23:45:14 -0300

    new request

--- lib/models/item.dart
+++ lib/models/item.dart
@@ -1,18 +1,54 @@
 class Item {
   String title;
-  bool done;
+  String explanation;
+  String hdurl;
+  String mediaType;
+  String serviceVersion;
+  String url;
 
-  Item({this.title, this.done});
+  Item({
+    this.title,
+    this.explanation,
+    this.hdurl,
+    this.mediaType,
+    this.serviceVersion,
+    this.url,
+  });
 
   Item.fromJson(Map<String, dynamic> json) {
     title = json['title'];
-    done = json['done'];
+    explanation = json['explanation'];
+    hdurl = json['hdurl'];
+    mediaType = json['mediaType'];
+    serviceVersion = json['serviceVersion'];
+    url = json['url'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['title'] = this.title;
-    data['done'] = this.done;
+    data['explanation'] = this.explanation;
+    data['hdurl'] = this.hdurl;
+    data['mediaType'] = this.mediaType;
+    data['serviceVersion'] = this.serviceVersion;
+    data['url'] = this.url;
     return data;
   }
 }
+
+// {
+//   "date":"2019-04-04",
+//   "explanation": "After the Crab Nebula, M1, this giant star cluster is the second entry in 18th
+//       century astronomer Charles Messier's famous list of things that are not comets. M2 is one of
+//       the largest globular star clusters now known to roam the halo of our Milky Way galaxy.
+//       Though Messier originally described it as a nebula without stars, this stunning Hubble
+//       image resolves stars across the central 40 light-years of M2. Its population of stars
+//       numbers close to 150,000, concentrated within a total diameter of around 175 light-years.
+//       About 55,000 light-years distant toward the constellation Aquarius, this ancient denizen
+//       of the Milky Way, also known as NGC 7089, is 13 billion years old.",
+//   "hdurl":"https://apod.nasa.gov/apod/image/1904/potw1913aa.jpg",
+//   "media_type":"image",
+//   "service_version":"v1",
+//   "title":"Messier 2",
+//   "url":"https://apod.nasa.gov/apod/image/1904/potw1913aM2_1024.jpg"
+//   }
--- lib/pages/apod_list.dart
+++ lib/pages/apod_list.dart
@@ -1,18 +1,40 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:nasa_apod_flutter/models/item.dart';
+import 'package:http/http.dart' as http;
+
 
 class ApodList extends StatefulWidget {
   var items = new List<Item>();
+
   var days = [];
 
   ApodList() {
     items = [];
 
-    items.add(Item(title: "test 1", done: false));
-    items.add(Item(title: "test 2", done: false));
-    items.add(Item(title: "test 3", done: false));
+    // items.add(Item(title: "test 1"));
+    // items.add(Item(title: "test 2"));
+    // items.add(Item(title: "test 3"));
 
     dateArray();
+    fetchItem("2019-12-1", "2019-12-17");
+  }
+
+  Future fetchItem(String startDate, String endDate) async {
+    // https://api.nasa.gov/planetary/apod?api_key=8g23BupBSJXtE86RIMPOYki0ele3dSRvoshr5yLM&start_date=2019-1-1&end_date=2019-12-17
+    final response = await http.get(
+        'https://api.nasa.gov/planetary/apod?api_key=8g23BupBSJXtE86RIMPOYki0ele3dSRvoshr5yLM&start_date=$startDate&end_date=$endDate');
+
+    if (response.statusCode == 200) {
+      // var ret = Item.fromJson(json.decode(response.body));
+      var ret = json.decode(response.body);
+      // If server returns an OK response, parse the JSON.
+      print(ret);
+      return ret;
+    } else {
+      // If that response was not OK, throw an error.
+      throw Exception('Failed to load post');
+    }
   }
 
   dateArray() {
@@ -22,12 +44,13 @@ class ApodList extends StatefulWidget {
     var mes = startDate.month.toInt();
     var dia = startDate.day.toInt();
 
-    while (ano >= 2017) {
+    while (ano >= 2019) {
       while (mes >= 1) {
         while (dia >= 1) {
           DateTime currentDate = DateTime(ano, mes, dia);
           if (currentDate != null) {
-            days.add("$ano-$mes-$dia");
+            var currentDay = "$ano-$mes-$dia";
+            days.add("$currentDay");
           }
           dia--;
         }
@@ -38,8 +61,6 @@ class ApodList extends StatefulWidget {
       ano--;
     }
   }
-
-  convertDate() {}
 
   @override
   _ApodListState createState() => _ApodListState();
@@ -55,12 +76,14 @@ class _ApodListState extends State<ApodList> {
       ),
       body: ListView.builder(
         itemCount: widget.items.length,
+        // itemCount: 1,
+        // itemCount: 20,
         itemBuilder: (BuildContext ctxt, int index) {
-          final item = widget.items[index];
+          // final item = widget.items[index];
+
           return Container(
             decoration: BoxDecoration(
               color: Colors.grey[200],
-              // border: Border.all(color: Colors.yellowAccent[700]),
               borderRadius: new BorderRadius.circular(8.0),
             ),
             margin: new EdgeInsets.symmetric(horizontal: 7.0, vertical: 7.0),
@@ -70,23 +93,11 @@ class _ApodListState extends State<ApodList> {
                 Stack(children: <Widget>[
                   ClipRRect(
                     child: Image.network(
-                      // 'http://www2.carosouvintes.org.br/wp-content/uploads/cat-pixabay-3442257_1280.jpg',
                       'https://apod.nasa.gov/apod/image/1906/StrawberryMoon_Chasiotis_960.jpg',
-                      // 'https://i.ytimg.com/vi/Hov5Sv8fBDY/hqdefault.jpg',
-                      // 'https://i.ytimg.com/vi/iAczTzD5Sko/maxresdefault.jpg',
                       fit: BoxFit.cover,
                     ),
                     borderRadius: new BorderRadius.circular(7.0),
                   ),
-                  // DecoratedBox(
-                  //   decoration: BoxDecoration(
-                  //     image: DecorationImage(
-                  //         image: NetworkImage(
-                  //             "https://apod.nasa.gov/apod/image/1906/StrawberryMoon_Chasiotis_960.jpg"),
-                  //         fit: BoxFit.cover),
-                  //   ),
-                  //   // child: Center(child: FlutterLogo(size: 300)),
-                  // ),
                   Center(
                     child: Align(
                       alignment: Alignment.centerRight,
@@ -98,7 +109,6 @@ class _ApodListState extends State<ApodList> {
                         decoration: BoxDecoration(
                           color: Colors.grey[200],
                           borderRadius: new BorderRadius.circular(18.0),
-                          // border: Border.all(color: Colors.yellowAccent[700]),
                         ),
                         child: Text(
                           "15/12/2019",
