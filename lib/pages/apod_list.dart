d822016d85340e246d850a475315e5e90e1094ec
maccali <guilherme.maccali@universo.univates.br>
2019-12-19 22:14:05 -0300

    New card

--- lib/pages/apod_list.dart
+++ lib/pages/apod_list.dart
@@ -2,16 +2,28 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:nasa_apod_flutter/models/item.dart';
 import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class ApodList extends StatefulWidget {
   // var items = new List<Item>();
   var items = [];
-  var startDate = "2019-12-1";
-  var endDate = "2019-12-17";
+  var startDate = "2019-12-14";
+  var endDate = "";
+  var limitReq = 5;
+  var countReq = 0;
+
+  // limite de 6 request IM APOD API per second (5)
+  // var endDate = new DateFormat("yMd");
 
   ApodList() {
+    var now = new DateTime.now();
+    var formatter = new DateFormat('yyyy-MM-dd');
+    var formatted = formatter.format(now);
+
+    endDate = formatted.toString();
+
     items = [];
-    items.add(Item(title: "111"));
   }
 
   @override
@@ -24,9 +36,9 @@ class _ApodListState extends State<ApodList> {
         "https://api.nasa.gov/planetary/apod?api_key=8g23BupBSJXtE86RIMPOYki0ele3dSRvoshr5yLM&start_date=$startDate&end_date=$endDate");
 
     if (res.statusCode == 200) {
-      var itemStr = res.body;
+      var itemsStr = res.body;
 
-      return itemStr;
+      return itemsStr;
     } else {
       throw ("some arbitrary error");
     }
@@ -46,12 +58,8 @@ class _ApodListState extends State<ApodList> {
               var valueMap = [];
               String dataItem = snapshot.data;
               valueMap = json.decode(dataItem);
-
-              // for (var f in valueMap) {
-              // setState(() {
-              //   widget.items = valueMap;
-              // });
-              // }
+              Iterable inReverseValueMap = valueMap.reversed;
+              valueMap = inReverseValueMap.toList();
 
               return ListView.builder(
                 itemCount: valueMap.length,
@@ -62,9 +70,9 @@ class _ApodListState extends State<ApodList> {
                   print(item['url']);
                   return Container(
                     decoration: BoxDecoration(
-                      color: Colors.grey[200],
-                      borderRadius: new BorderRadius.circular(8.0),
-                    ),
+                        color: Colors.grey[200],
+                        borderRadius: new BorderRadius.circular(8.0),
+                        border: Border.all(color: Colors.deepPurple)),
                     margin: new EdgeInsets.symmetric(
                         horizontal: 7.0, vertical: 7.0),
                     padding: new EdgeInsets.symmetric(
@@ -74,9 +82,9 @@ class _ApodListState extends State<ApodList> {
                         Stack(children: <Widget>[
                           (item["media_type"] == "image")
                               ? ClipRRect(
-                                  child: Image.network(
-                                    item['url'],
-                                    // 'https://apod.nasa.gov/apod/image/1906/StrawberryMoon_Chasiotis_960.jpg',
+                                  child: FadeInImage.memoryNetwork(
+                                    placeholder: kTransparentImage,
+                                    image: item['url'],
                                     fit: BoxFit.cover,
                                   ),
                                   borderRadius: new BorderRadius.circular(7.0),
@@ -91,11 +99,13 @@ class _ApodListState extends State<ApodList> {
                                 padding: new EdgeInsets.symmetric(
                                     horizontal: 8.0, vertical: 5.0),
                                 decoration: BoxDecoration(
-                                  color: Colors.grey[200],
-                                  borderRadius: new BorderRadius.circular(18.0),
-                                ),
+                                    color: Colors.grey[200],
+                                    borderRadius:
+                                        new BorderRadius.circular(18.0),
+                                    border:
+                                        Border.all(color: Colors.deepPurple)),
                                 child: Text(
-                                  "15/12/2019",
+                                  item["date"],
                                   style: TextStyle(
                                     // fontWeight: FontWeight.bold,
                                     fontSize: 15,
@@ -108,6 +118,8 @@ class _ApodListState extends State<ApodList> {
                         Align(
                           alignment: Alignment.centerLeft,
                           child: Container(
+                            margin:
+                                const EdgeInsets.only(top: 10.0, bottom: 6.0),
                             // color: Colors.red,
                             child: Text(
                               item['title'],
@@ -116,13 +128,6 @@ class _ApodListState extends State<ApodList> {
                                 fontSize: 18,
                               ),
                             ),
-                          ),
-                        ),
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: Container(
-                            // color: Colors.red,
-                            child: Text("Descrição"),
                           ),
                         ),
                       ],
@@ -139,84 +144,9 @@ class _ApodListState extends State<ApodList> {
                   ),
                 );
               }
-              print("?");
               return CircularProgressIndicator();
             }
           },
-        )
-        // body: ListView.builder(
-        //   itemCount: widget.items.length,
-        //   itemBuilder: (BuildContext ctxt, int index) {
-        //     final item = widget.items[index];
-        //     // print(widget.items.length);
-
-        //     print(widget.items.length);
-        //     print(widget.items);
-        //     return Container(
-        //       decoration: BoxDecoration(
-        //         color: Colors.grey[200],
-        //         borderRadius: new BorderRadius.circular(8.0),
-        //       ),
-        //       margin: new EdgeInsets.symmetric(horizontal: 7.0, vertical: 7.0),
-        //       padding: new EdgeInsets.symmetric(horizontal: 7.0, vertical: 7.0),
-        //       child: Column(
-        //         children: <Widget>[
-        //           Stack(children: <Widget>[
-        //             ClipRRect(
-        //               child: Image.network(
-        //                 'https://apod.nasa.gov/apod/image/1906/StrawberryMoon_Chasiotis_960.jpg',
-        //                 fit: BoxFit.cover,
-        //               ),
-        //               borderRadius: new BorderRadius.circular(7.0),
-        //             ),
-        //             Center(
-        //               child: Align(
-        //                 alignment: Alignment.centerRight,
-        //                 child: Container(
-        //                   margin: new EdgeInsets.symmetric(
-        //                       horizontal: 6.0, vertical: 6.0),
-        //                   padding: new EdgeInsets.symmetric(
-        //                       horizontal: 8.0, vertical: 5.0),
-        //                   decoration: BoxDecoration(
-        //                     color: Colors.grey[200],
-        //                     borderRadius: new BorderRadius.circular(18.0),
-        //                   ),
-        //                   child: Text(
-        //                     "15/12/2019",
-        //                     style: TextStyle(
-        //                       // fontWeight: FontWeight.bold,
-        //                       fontSize: 15,
-        //                     ),
-        //                   ),
-        //                 ),
-        //               ),
-        //             ),
-        //           ]),
-        //           Align(
-        //             alignment: Alignment.centerLeft,
-        //             child: Container(
-        //               // color: Colors.red,
-        //               child: Text(
-        //                 "Título",
-        //                 style: TextStyle(
-        //                   fontWeight: FontWeight.bold,
-        //                   fontSize: 18,
-        //                 ),
-        //               ),
-        //             ),
-        //           ),
-        //           Align(
-        //             alignment: Alignment.centerLeft,
-        //             child: Container(
-        //               // color: Colors.red,
-        //               child: Text("Descrição"),
-        //             ),
-        //           ),
-        //         ],
-        //       ),
-        //     );
-        //   },
-        // ),
-        );
+        ));
   }
 }
